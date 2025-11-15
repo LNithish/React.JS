03c6def8abcfd03481d44c024a0e011843c34684
LNithish <nithishkumarvip@gmail.com>
2025-11-15 09:16:00 +0530

    List with Keys

---
+++ AdvancedFeatures/ListsWithKey/ListsKey.js
@@ -0,0 +1,34 @@
+
+class NamesList extends React.Component{
+
+    render() {
+        // TODO while recording: Show each of these in turn
+
+        // TODO First show this
+        // var listItems = this.props.names.map(
+        //     (name) => <li key={1}>{name.toUpperCase()}</li>
+        // );
+
+        // TODO Second show this
+        // dont use index as key if your list might change
+        // var listItems = this.props.names.map(
+        //     (name) => <li key={name}>{name.toUpperCase()}</li>
+        // );
+
+        // TODO Third show this
+        var listItems = this.props.names.map(
+            (name, index) => <li key={index}>{name.toUpperCase()}</li>
+        );
+    
+        return(
+            <ul className = "names">{listItems}</ul>
+        );
+    }
+}
+
+
+const names = ['Esther', 'Lily', 'Candace', 'Edward', 'Sidney'];
+
+
+ReactDOM.render(<NamesList names={names} />,
+    document.getElementById('react-lists'));
